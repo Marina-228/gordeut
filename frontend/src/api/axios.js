29b8a2d26e83bf342138ea24340f53e417f75fc0
miss <gordeyukm@gmail.com>
2026-06-03 13:40:55 +0300

    fix: update api url to env variable

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({ 
-  baseURL: 'https://vercel.com/marina-s-projects12/gordeut-rzle/CbQPmBH5bpeyxSzpNxWmYorc8hPC',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
   withCredentials: true
 });
 
